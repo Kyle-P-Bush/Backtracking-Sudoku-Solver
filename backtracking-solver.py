8bcffb07a1e1d7ec46eed332da8558fbbc5d83b9
Kyle-P-Bush <kylebush64@gmail.com>
2022-11-17 21:23:53 -0600

    Working logic to solve and print the puzzle. Still need to add some logic comments and clean up the printing format.

--- backtracking-solver.py
+++ backtracking-solver.py
@@ -12,6 +12,20 @@ board = [
             [0,0,7,0,0,6,0,0,1],
             [0,4,0,0,8,0,0,0,9]
         ]
+def solve(board):
+    find = findEmpty(board)
+    if not find:
+        return True
+    else:
+        row, column = find
+    for i in range(1,10):
+        if isValid(board, i, (row, column)):
+            board[row][column] = i
+
+            if solve(board):
+                return True
+            board[row][column] = 0
+    return False
 
 #Check for a valid number based on the constrictions.
 #Takes in the board, a number to try, and the position within the board.
@@ -24,20 +38,26 @@ def isValid(board, number, position):
         #Checks each column within a row (pos will be a tuple (row, column)) and sees if it is equal to
         #the number that was just added. However, the position[1] != i makes sure that we skip the position
         #we just added the number to since theres no need to check that.
-        if board[position[0]][i] == num and position[1] != i:
+        if board[position[0]][i] == number and position[1] != i:
             return False
     
 
     #Check if the number is in the column
     
     for i in range(len(board)):
-        if board[i][position[1]] == num and position[0] != i:
+        if board[i][position[1]] == number and position[0] != i:
             return False
 
     #Check if the number is in the 3x3 Block
-
-
-
+    box_x = position[1] // 3
+    box_y = position[0] // 3
+    #TODO: Add description of logic
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 +3):
+            if board[i][j] == number and (i,j) != position:
+                return False
+    
+    return True
 
 #Walks through each row to print the board. Can be improved to look more user friendly later but not necessary.
 def printBoard(board):
@@ -52,4 +72,9 @@ def findEmpty(board):
         for column in range(len(board[0])):
             if board[row][column] == 0:
                 return (row, column)
+    return None
 
+printBoard(board)
+solve(board)
+print("---------")
+printBoard(board) 
